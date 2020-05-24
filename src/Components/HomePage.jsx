f362346dfbf5278c176a56c5b0897d80e3e2c02e
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-05-24 19:45:33 +0200

    revert

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import '../Global.css';
-import image from '../Globals/LogoRaw.png'
+import image from '../Globals/linkedin.png'
 
 const mapStateToProps = state => ({
   ...state
@@ -15,10 +15,7 @@ class HomePage extends Component {
    return (
     <div className="home">
       <div className="homeHeader">
-        <div className="homeHeaderTitle">
-          <img src={image} className="homeHeaderImage" alt=""/>
-          Felicitas Deutges
-        </div>
+        Felicitas Deutges
       </div>
       <div className="homeNavBar">
         <div className="homeNavBarButtons">
@@ -27,7 +24,7 @@ class HomePage extends Component {
           <a href="#projects" style={{color:"white"}}>Projects</a>
           <a href="#skills" style={{color:"white"}}>Skills</a>
           <a href="#contact" style={{color:"white"}}>Contact</a>
-          <a href="https://drive.google.com/file/d/19WHt13GpigzjlSWyPR5IpdWayys2wx32/view?usp=sharing" target="_blank" rel="noopener noreferrer" style={{color:"white"}}>Resume</a>
+          <a href="#contact" style={{color:"white"}}>-> Resume</a>
         </div>
       </div>
       <div className="me"><a name="me">Me</a></div>
@@ -36,7 +33,7 @@ class HomePage extends Component {
       <div className="skills"><a name="skills">Skills</a></div>
       <div className="contact"><a name="contact">Contact</a></div>
       <div className="homeFooter">
-        <a href="#me" style={{color:"white"}}>Back to top</a>
+        <a href="" style={{color:"white"}}>Back to top</a>
       </div>
     </div>
    );
