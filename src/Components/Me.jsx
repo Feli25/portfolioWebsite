56cd296fec9607c49b4d1a737140cbbd67e07808
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-05-25 19:21:29 +0200

    me page

--- src/Components/Me.jsx
+++ src/Components/Me.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import '../Global.scss';
+import feliPic from '../Globals/meComplete.jpg';
 
 const mapStateToProps = state => ({
   ...state
@@ -13,8 +14,24 @@ class Me extends Component {
    }
   render() {
     return(
-      <div className="me"><a name="me">Me</a></div>
-      
+      <section id="me">
+          <div  class="title">
+            <h2>About me</h2>
+          </div>
+          <article>
+              <div>
+                <p>
+                <h3>Hello!</h3>
+                My name is Felicitas Deutges, and I am a Berlin based Fullstack Web Developer. 
+                I primarily user JavaScript, with React and Node.js, and SQL. But picking up new frameworks or technologies is not a problem.<br/>
+                I enjoy building everything, from small business sites to rich interactive web apps. If you are a business seaking a web presence or an employer looking to hire, you can get in touch with me <a href="#contact">here</a>.
+                </p>
+              </div>
+              <div>
+                <img src={feliPic} width='90%' alt=""/>
+              </div>
+          </article>
+        </section>
    );
   }
  }
