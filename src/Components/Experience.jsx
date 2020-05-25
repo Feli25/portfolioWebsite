cdf4a859d525692bb4ce6cdf0e8a776f03fef322
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-05-25 19:28:16 +0200

    add experience section

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -13,8 +13,30 @@ class Experience extends Component {
    }
   render() {
     return(
-      <div className="experience"><a name="experience">Experience</a></div>
-      
+      <section id="experience">
+          <div class="title">
+            <h2>Experience</h2>
+          </div>
+          <article>
+              <div>
+              <p>
+                <h3>Work Experience</h3>
+                Currently I am full time employed, working on Full Stack Projects in React, Node and SQL. 
+                I have gained extensive experience with applications and the development process in a fast changing environment with real-life products, developed multiple user interfaces and complex logics in backend and frontend.
+                Besides developing I have been able to take over Project Managment tasks and hiring new members for our team. 
+                I have been doing code reviews for all of my team members and been architectural lead in a few of our biggest projects.
+                </p>
+              </div>
+              <div>
+                <p>
+                <h3>Side Projects</h3>
+                As you can see in the next section, I am also constantly looking for challenges when working on side projects. 
+                By creating fun and real projects I can learn new technologies or deepen my understanding for technologies I already know.
+                The list of side projects is constantly growing.
+                </p>
+              </div>
+            </article>
+        </section>
    );
   }
  }
