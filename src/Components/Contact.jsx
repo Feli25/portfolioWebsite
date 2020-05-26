c0758d2a5085784f4c400a802ddac9add2d60d43
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-05-26 09:28:55 +0200

    add recaptcha code

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import '../Global.scss';
 import mailbox from '../Globals/mailBox.jpg';
 import emailjs from 'emailjs-com';
+import Recaptcha from "react-recaptcha";
 
 const mapStateToProps = state => ({
   ...state
@@ -47,49 +48,61 @@ class Contact extends Component {
         }})
       })
   }
-  render() {var {fname, lname, mail, subject, message, errMessage, captchaVerified} = this.props.simpleReducer
-  return(
-      <section id="contact">
-        <div class="title">
-          <h2>Contact</h2>
-        </div>
-        <div className="contact">
-          <div className="wrapForm">
-            <h3>Contact</h3>
-            <p>For all pricing or faq, please fill out the form below</p>
-            <div>
-              <p>Name <span>&#42;</span></p>
-              <div className='formNameWrap'>
-                <input placeholder="First Name" name="fname" value={fname} onChange={this.updateStore}/>
-                <input placeholder="Last Name" name="lname" value={lname} onChange={this.updateStore}/>
-              </div>
-            </div>
-            <div>
-              <p>Email Address <span>&#42;</span></p>
-              <input placeholder="Email Address" name="mail" value={mail} onChange={this.updateStore}/>
-            </div>
-            <div>
-              <p>Subject <span>&#42;</span></p>
-              <input placeholder="Subject" name="subject" value={subject} onChange={this.updateStore}/>
-            </div>
-            <div>
-              <p>Type your message <span>&#42;</span></p>
-              <textarea placeholder="Message" name="message" value={message} onChange={this.updateStore}/>
-            </div>
-            <div>
-              <button onClick={this.submitContact}
-              // disabled={!captchaVerified}
-              >Submit</button>
-            </div>
-            {errMessage && <div>{errMessage}</div>}
-          </div>
-          <div className="contactImg">
-            <img src={mailbox} className='img' alt=""/>
-          </div>
-        </div>
-      </section>
-    ); 
+  verifiedCaptcha=(response)=>{
+    if(response){
+        this.props.dispatch({type:"UPDATE_STORE", name:"captchaVerified", value:true})
+    }
   }
+  render() {
+    var {fname, lname, mail, subject, message, errMessage, captchaVerified} = this.props.simpleReducer
+      return(
+        <section id="contact">
+          <div class="title">
+            <h2>Contact</h2>
+          </div>
+          <div className="contact">
+            <div className="wrapForm">
+              <h3>Contact</h3>
+              <p>For all pricing or faq, please fill out the form below</p>
+              <div>
+                <p>Name <span>&#42;</span></p>
+                <div className='formNameWrap'>
+                  <input placeholder="First Name" name="fname" value={fname} onChange={this.updateStore}/>
+                  <input placeholder="Last Name" name="lname" value={lname} onChange={this.updateStore}/>
+                </div>
+              </div>
+              <div>
+                <p>Email Address <span>&#42;</span></p>
+                <input placeholder="Email Address" name="mail" value={mail} onChange={this.updateStore}/>
+              </div>
+              <div>
+                <p>Subject <span>&#42;</span></p>
+                <input placeholder="Subject" name="subject" value={subject} onChange={this.updateStore}/>
+              </div>
+              <div>
+                <p>Type your message <span>&#42;</span></p>
+                <textarea placeholder="Message" name="message" value={message} onChange={this.updateStore}/>
+              </div>
+              <Recaptcha
+                sitekey="6LcTrPgUAAAAAFfn6TLUm0erW9CTzXPXqnZ9Zvrr"
+                render="explicit"
+                verifyCallback={this.verifiedCaptcha}
+                onloadCallback={()=>{console.log("HAS LOADED CAPTCHA")}}
+              />
+              <div>
+                <button onClick={this.submitContact}
+                disabled={!captchaVerified}
+                >Submit</button>
+              </div>
+              {errMessage && <div>{errMessage}</div>}
+            </div>
+            <div className="contactImg">
+              <img src={mailbox} className='img' alt=""/>
+            </div>
+          </div>
+        </section>
+      ); 
+    }
  }
  
 
