8a74546f013ef70d1d0bcf3f0ce16438c1af0086
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-10-13 19:26:09 +0200

    roll back to heroku version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
-// import Layout from './Components/Layout'
-import HomePage from './Components/HomePage'
-// import ExampleApp from './Components/ExampleApp'
+import Layout from './Components/Layout'
 
 
 class App extends Component {
   render() {
    return (
     <div className="App">
-     {/* <Layout/> */}
-     <HomePage/>
-     {/* <ExampleApp/> */}
+     <Layout/>
     </div>
    );
   }
--- src/Components/Divider/Divider.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style.scss';
-
-export default function Divider(){
-    return<>
-       <div class="divider"><span></span><span>&#10038;</span><span></span></div>
-        </>
-};
